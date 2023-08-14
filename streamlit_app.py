f7b584ff39e58431ab7016eb9b79a321ee0e4899
pyangx19 <142197499+pyangx19@users.noreply.github.com>
2023-08-14 19:28:47 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy Diner')
+streamlit.header('Breakfast Menu')
+stremalit.text('Omega 3 & Blueberry Oatmeal')
+stremalit.text('Kale, Spinach & Rocket Smoothie')
+stremalit.text('Hard-boiled Free-Range Egg')
